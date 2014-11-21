164d580ac73ab463a16b4d223a84bcd8fb8e6c0a
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-11-21 10:09:47 +0530

    Pep8ified

--- trial.py
+++ trial.py
@@ -2,21 +2,27 @@ import commands
 import os
 import eyed3
 
-#Changing string to unicode
+# Changing string to unicode
+
+
 def str_uni(string):
-    unicode = unicode(string,'utf-8')
+    unicode = unicode(string, 'utf-8')
     return unicode
 
+
 def addslashes(s):
-    s=s.replace(' ','\ ').replace("'","\'").replace('"','\"')
+    s = s.replace(' ', '\ ').replace("'", "\'").replace('"', '\"')
     return s
 
 # I used a class here
-# This is pretty surprising since I suck at OOPS 
+# This is pretty surprising since I suck at OOPS
 # Looks like I want to conquer my Fear
 # Carpe Diem
 # Also , Fuck you TKT
+
+
 class Trial(object):
+
     def __init__(self):
         self.message = 'is it a constructor'
 
@@ -29,23 +35,25 @@ class Trial(object):
 # Official Docs
 # http://eyed3.nicfit.net/api/eyed3.id3.html#id1
 
-#Change method of taking input , use some file - explorer
+# Change method of taking input , use some file - explorer
 #music_path = str(raw_input("Enter the path for the Music Directory: "))
 music_path = '/home/yashmehrotra/Music/Music/'
 os.chdir(music_path)
 current_directory = commands.getoutput('pwd')
-mp3_list = commands.getoutput('ls -R | grep ".mp3" ').split('\n') # Finding the right bash command took 30 mins , it isnt as easy as it seems
+# Finding the right bash command took 30 mins , it isnt as easy as it seems
+mp3_list = commands.getoutput('ls -R | grep ".mp3" ').split('\n')
 
-#List of all the music files in the given directory
-# Be a true pythonista and do not use this -- edit - fixed i am a true pythonista \n Me Gusta (Why did that troll went out of fashion)
+# List of all the music files in the given directory
+# Be a true pythonista and do not use this -- edit - fixed i am a true
+# pythonista \n Me Gusta (Why did that troll went out of fashion)
 print "Here is a list of all the songs"
-for index,song in enumerate(mp3_list, start=1):
-    print index,song
+for index, song in enumerate(mp3_list, start=1):
+    print index, song
 
 print "Please enter the song no. you want to edit,press -1 to exit"
 user_choice = int(raw_input())
 if user_choice != -1:
-    x = mp3_list[user_choice-1]
+    x = mp3_list[user_choice - 1]
     str1 = 'find ' + current_directory + ' -name "' + x + '*"'
     str1 = 'find %s -name "%s*" ' % (current_directory, x)
     # Improve fucking str1 , have u ever heard of string formatting asshole
@@ -53,5 +61,5 @@ if user_choice != -1:
     x = commands.getoutput(str1)
     print x
     audio_file = eyed3.load(x)
-    t=Trial()
+    t = Trial()
     t.show_details(audio_file)
